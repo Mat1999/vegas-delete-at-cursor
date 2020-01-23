651ffa2313a8cfeedecbf6bc4c4af2a692639f7d
Mat1999 <50171708+Mat1999@users.noreply.github.com>
2020-01-23 15:56:56 +0100

    Add files via upload

---
+++ basic/MyClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using ScriptPortal.Vegas;
+
+namespace deleteatcursor
+{
+	/// <summary>
+	/// This script deletes every event under the cursor in Vegas Pro
+	/// </summary>
+	public class EntryPoint{
+		public void FromVegas(Vegas myVegas){
+			try{
+				foreach (Track myTrack in myVegas.Project.Tracks){//goes through each track's each event
+					foreach (TrackEvent myEvent in myTrack.Events) {
+						if ((myEvent.Start.FrameCount <= myVegas.Cursor.FrameCount) && (myEvent.End.FrameCount > myVegas.Cursor.FrameCount)){//if the event is under the cursor, or starts at the cursor
+							myTrack.Events.Remove(myEvent);//it gets removed
+						}
+					}
+				}
+			}
+			catch{
+				
+			}
+		}
+	}
+}
