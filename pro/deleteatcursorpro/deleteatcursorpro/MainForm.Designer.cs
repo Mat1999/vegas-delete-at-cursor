0b1f6e8af88c5bf57e78b7a4dd33a5a8a7cf84b0
Mat1999 <50171708+Mat1999@users.noreply.github.com>
2020-02-29 01:07:19 +0100

    Pro version

---
+++ pro/deleteatcursorpro/deleteatcursorpro/MainForm.Designer.cs
@@ -0,0 +1,233 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Matyi
+ * Date: 2020.01.23.
+ * Time: 16:24
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace deleteatcursorpro
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button btn_terminate;
+		private System.Windows.Forms.CheckBox chb_split;
+		private System.Windows.Forms.RadioButton rdb_type1;
+		private System.Windows.Forms.RadioButton rdb_type2;
+		private System.Windows.Forms.RadioButton rdb_type3;
+		private System.Windows.Forms.PictureBox pbt_setting;
+		private System.Windows.Forms.TextBox tbx_preview;
+		private System.Windows.Forms.ComboBox cbb_selectshort;
+		private System.Windows.Forms.Button btn_savekey;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Panel panel1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btn_terminate = new System.Windows.Forms.Button();
+			this.chb_split = new System.Windows.Forms.CheckBox();
+			this.rdb_type1 = new System.Windows.Forms.RadioButton();
+			this.rdb_type2 = new System.Windows.Forms.RadioButton();
+			this.rdb_type3 = new System.Windows.Forms.RadioButton();
+			this.pbt_setting = new System.Windows.Forms.PictureBox();
+			this.tbx_preview = new System.Windows.Forms.TextBox();
+			this.cbb_selectshort = new System.Windows.Forms.ComboBox();
+			this.btn_savekey = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			((System.ComponentModel.ISupportInitialize)(this.pbt_setting)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// btn_terminate
+			// 
+			this.btn_terminate.FlatAppearance.BorderColor = System.Drawing.SystemColors.Highlight;
+			this.btn_terminate.FlatAppearance.BorderSize = 3;
+			this.btn_terminate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btn_terminate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.btn_terminate.Location = new System.Drawing.Point(12, 60);
+			this.btn_terminate.Name = "btn_terminate";
+			this.btn_terminate.Size = new System.Drawing.Size(84, 28);
+			this.btn_terminate.TabIndex = 0;
+			this.btn_terminate.Text = "Delete";
+			this.btn_terminate.UseVisualStyleBackColor = true;
+			this.btn_terminate.Click += new System.EventHandler(this.Btn_terminateClick);
+			// 
+			// chb_split
+			// 
+			this.chb_split.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.chb_split.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.chb_split.Location = new System.Drawing.Point(12, 13);
+			this.chb_split.Name = "chb_split";
+			this.chb_split.Size = new System.Drawing.Size(104, 24);
+			this.chb_split.TabIndex = 1;
+			this.chb_split.Text = "Split delete";
+			this.chb_split.UseVisualStyleBackColor = true;
+			this.chb_split.CheckedChanged += new System.EventHandler(this.Chb_splitCheckedChanged);
+			// 
+			// rdb_type1
+			// 
+			this.rdb_type1.FlatAppearance.CheckedBackColor = System.Drawing.Color.Red;
+			this.rdb_type1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.rdb_type1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.rdb_type1.Location = new System.Drawing.Point(159, 12);
+			this.rdb_type1.Name = "rdb_type1";
+			this.rdb_type1.Size = new System.Drawing.Size(104, 24);
+			this.rdb_type1.TabIndex = 2;
+			this.rdb_type1.TabStop = true;
+			this.rdb_type1.Text = "Just Audio";
+			this.rdb_type1.UseVisualStyleBackColor = true;
+			this.rdb_type1.Click += new System.EventHandler(this.Rdb_type1Click);
+			// 
+			// rdb_type2
+			// 
+			this.rdb_type2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.rdb_type2.Location = new System.Drawing.Point(159, 42);
+			this.rdb_type2.Name = "rdb_type2";
+			this.rdb_type2.Size = new System.Drawing.Size(104, 24);
+			this.rdb_type2.TabIndex = 3;
+			this.rdb_type2.TabStop = true;
+			this.rdb_type2.Text = "Just Video";
+			this.rdb_type2.UseVisualStyleBackColor = true;
+			this.rdb_type2.Click += new System.EventHandler(this.Rdb_type2Click);
+			// 
+			// rdb_type3
+			// 
+			this.rdb_type3.Checked = true;
+			this.rdb_type3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.rdb_type3.Location = new System.Drawing.Point(159, 76);
+			this.rdb_type3.Name = "rdb_type3";
+			this.rdb_type3.Size = new System.Drawing.Size(104, 24);
+			this.rdb_type3.TabIndex = 4;
+			this.rdb_type3.TabStop = true;
+			this.rdb_type3.Text = "Video and Audio";
+			this.rdb_type3.UseVisualStyleBackColor = true;
+			this.rdb_type3.Click += new System.EventHandler(this.Rdb_type3Click);
+			// 
+			// pbt_setting
+			// 
+			this.pbt_setting.Image = global::deleteatcursorpro.Resource1.settings;
+			this.pbt_setting.Location = new System.Drawing.Point(262, 7);
+			this.pbt_setting.Name = "pbt_setting";
+			this.pbt_setting.Size = new System.Drawing.Size(28, 29);
+			this.pbt_setting.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pbt_setting.TabIndex = 5;
+			this.pbt_setting.TabStop = false;
+			this.pbt_setting.Click += new System.EventHandler(this.Pbt_settingClick);
+			this.pbt_setting.MouseEnter += new System.EventHandler(this.Pbt_settingMouseEnter);
+			this.pbt_setting.MouseLeave += new System.EventHandler(this.Pbt_settingMouseLeave);
+			// 
+			// tbx_preview
+			// 
+			this.tbx_preview.Enabled = false;
+			this.tbx_preview.Location = new System.Drawing.Point(17, 48);
+			this.tbx_preview.Name = "tbx_preview";
+			this.tbx_preview.Size = new System.Drawing.Size(138, 20);
+			this.tbx_preview.TabIndex = 6;
+			this.tbx_preview.Text = "None + None";
+			// 
+			// cbb_selectshort
+			// 
+			this.cbb_selectshort.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.cbb_selectshort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbb_selectshort.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.cbb_selectshort.FormattingEnabled = true;
+			this.cbb_selectshort.Items.AddRange(new object[] {
+			"Split",
+			"Just Audio",
+			"Just Video",
+			"Video and Audio"});
+			this.cbb_selectshort.Location = new System.Drawing.Point(17, 23);
+			this.cbb_selectshort.Name = "cbb_selectshort";
+			this.cbb_selectshort.Size = new System.Drawing.Size(138, 21);
+			this.cbb_selectshort.TabIndex = 7;
+			this.cbb_selectshort.SelectedIndexChanged += new System.EventHandler(this.Cbb_selectshortSelectedIndexChanged);
+			// 
+			// btn_savekey
+			// 
+			this.btn_savekey.FlatAppearance.BorderColor = System.Drawing.SystemColors.Highlight;
+			this.btn_savekey.FlatAppearance.BorderSize = 3;
+			this.btn_savekey.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btn_savekey.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.btn_savekey.Location = new System.Drawing.Point(43, 77);
+			this.btn_savekey.Name = "btn_savekey";
+			this.btn_savekey.Size = new System.Drawing.Size(84, 28);
+			this.btn_savekey.TabIndex = 8;
+			this.btn_savekey.Text = "Save";
+			this.btn_savekey.UseVisualStyleBackColor = true;
+			this.btn_savekey.Click += new System.EventHandler(this.Btn_savekeyClick);
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.label1.Location = new System.Drawing.Point(17, 2);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(138, 18);
+			this.label1.TabIndex = 9;
+			this.label1.Text = "Custom shortcuts";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.panel1.Controls.Add(this.btn_savekey);
+			this.panel1.Controls.Add(this.tbx_preview);
+			this.panel1.Controls.Add(this.cbb_selectshort);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Location = new System.Drawing.Point(322, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(172, 118);
+			this.panel1.TabIndex = 10;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.ClientSize = new System.Drawing.Size(492, 116);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.pbt_setting);
+			this.Controls.Add(this.rdb_type3);
+			this.Controls.Add(this.rdb_type2);
+			this.Controls.Add(this.rdb_type1);
+			this.Controls.Add(this.chb_split);
+			this.Controls.Add(this.btn_terminate);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = global::deleteatcursorpro.Resource1.scripticon;
+			this.KeyPreview = true;
+			this.Name = "MainForm";
+			this.Text = "Delete at Cursor PRO Edition (LOL)";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainFormKeyDown);
+			((System.ComponentModel.ISupportInitialize)(this.pbt_setting)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ pro/deleteatcursorpro/deleteatcursorpro/MainForm.cs
@@ -0,0 +1,322 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Matyi
+ * Date: 2020.01.23.
+ * Time: 16:24
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using ScriptPortal.Vegas;
+using System.IO;
+
+namespace deleteatcursorpro
+{
+
+	public partial class MainForm : Form
+	{
+		//constans to make the identification of certain elements easier
+		public const int SName = 0;
+		public const int SMod = 1;
+		public const int SKey = 2;
+		public string[] wrongkeys = {"None","Menu","ControlKey","ShiftKey",""};
+		//variables used int the form
+		public int type = 3;//what type of events will be deleted
+		public bool split = false;//is splitting when deleting turned on or off
+		public bool settkey = false;//are we modifying the settings, in the case of this script the shortcuts
+		//variables for filehandling
+		string savepath;
+		string copypath;
+		FileStream savefile;
+		StreamReader fileread;
+		StreamWriter filewrite;
+		//variables for handling keyboard inputs/shortcuts
+		string keymodact;//the currently selected modifier key (like control,shift,etc.) for the shortcut
+		string keybaseact;//the currently selected main key (like A,B,1,etc.) for the shortcut
+		string temp;
+		public struct Shorts{//this structure will hold the individual shortcuts
+			public string keyname;//the name of the function which the shortcut will activate
+			public string keymod;//the modifier key
+			public string keybase;//key main shortcut key
+			public Shorts(string name, string mod, string basic){
+				keyname = name;
+				keymod = mod;
+				keybase = basic;
+			}
+		}
+		List<Shorts> shortcuts = new List<Shorts>();//this list will hold all of the shortcuts used in the script
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			//TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void MainFormLoad(object sender, EventArgs e)//this is the first section that runs, happens when the windows opens up
+		{
+			this.Width = 312;
+			cbb_selectshort.SelectedIndex = 0;
+			savepath = Application.StartupPath + "\\script_shortcut.ini";//we set the savefiles location. It will be the scipt's location, plus the filename. 
+			copypath = Application.StartupPath + "\\tempshortcut.ini";//the backup copy's location
+			keymodact = "None";//resetting the currently selected modifier key 
+			keybaseact = "None";//resetting the currently selected base key
+			if (!File.Exists(savepath)){//if we don't have a shoortcut savefile, a default one is created
+				savefile = new FileStream(savepath,FileMode.CreateNew);
+				filewrite = new StreamWriter(savefile);
+				filewrite.WriteLine("DeleteAtCursor");
+				filewrite.WriteLine("Split;None;K");
+				filewrite.WriteLine("Just Audio;None;U");
+				filewrite.WriteLine("Just Video;None;I");
+				filewrite.WriteLine("Video and Audio;None;O");
+				filewrite.WriteLine("EndSection");
+				filewrite.Close();
+				savefile.Close();
+			}
+			savefile = new FileStream(savepath,FileMode.OpenOrCreate);//opening the savefile
+			fileread = new StreamReader(savefile);
+			//Reading in the shortcuts
+			while (fileread.EndOfStream == false){//going through each line of the savefile
+				temp = fileread.ReadLine();//reading it to a temporary variable
+				string[] templist = temp.Split(';');//the savefile separates the different data in one line with a ; so we split the line to it's individual elements
+				if (templist[SName] == "DeleteAtCursor"){//if we find the currently ued scripts section
+					temp = fileread.ReadLine();
+					templist = temp.Split(';');
+					while (templist[SName] != "EndSection"){//we read in the data to the shortcut's variable until we reach the end of the script's section in the savefile
+						shortcuts.Add(new Shorts(templist[SName],templist[SMod],templist[SKey]));
+						temp = fileread.ReadLine();
+						templist = temp.Split(';');
+					}
+					break;//we can end the reading because we've found our precious shortcuts
+				}
+			}
+			fileread.Close();//we close the file
+			savefile.Close();
+		}
+		
+		void End_Script(){//closes the window
+			split = chb_split.Checked;
+			this.DialogResult = DialogResult.OK;
+			this.Close();
+		}
+		
+		void Btn_terminateClick(object sender, EventArgs e)//closes the window when you press the button. Phew, this was hard to figure out.
+		{
+			End_Script();
+		}
+		void Btn_savekeyClick(object sender, EventArgs e)//runs when we try to save our custom shortcuts
+		{
+			bool keysok = true;//turns false if our custom shortcut is invalid
+			foreach (string incorrect in wrongkeys) {//we check if the currently selected shortcut keys are valid
+				if (incorrect == keybaseact){
+					keysok = false;
+				}
+			}
+			if (keysok == true){//if the keys are valid
+				savefile = new FileStream(savepath,FileMode.OpenOrCreate);//we open the file, the create is there to avoid problems if we don't have a file. Wait... I'm not sure I thought it through
+				fileread = new StreamReader(savefile);
+				File.Copy(savepath,copypath,true);//we create  a copy of the shortcut file before doing anything, so we can restore it if needed
+				//saving the keys in the file
+				try{
+					List<string> templist = new List<string>();
+					//reading in every shortcut from the savefile to a temporary list
+					while (fileread.EndOfStream != true) {
+						templist.Add(fileread.ReadLine());
+					}
+					fileread.Close();
+					savefile.Close();
+					savefile = new FileStream(savepath,FileMode.Truncate);
+					filewrite = new StreamWriter(savefile);
+					//reading the lines from the temporary list, and if they are the script's shortcuts
+					//we save them in the shortcuts list
+					for (int i = 0; i < templist.Count;i++) {
+						if (templist[i] == "DeleteAtCursor"){
+							while (templist[i] != "EndSection"){
+								string[] tempstring = templist[i].Split(';');
+								if (tempstring[SName] == cbb_selectshort.GetItemText(cbb_selectshort.SelectedItem)){
+									filewrite.WriteLine(tempstring[SName] + ";" + keymodact + ";" + keybaseact);
+									for (int j = 0; j < shortcuts.Count; j++) {
+										if (shortcuts[j].keyname == tempstring[SName]){
+											shortcuts[j] = new Shorts(tempstring[SName],keymodact,keybaseact);
+										}
+									}
+								}
+								else{
+									filewrite.WriteLine(templist[i]);
+								}
+								i++;
+							}
+							filewrite.WriteLine(templist[i]);
+						}
+						else{
+							filewrite.WriteLine(templist[i]);
+						}
+					}
+					filewrite.Close();
+					savefile.Close();
+					File.Delete(copypath);
+				}
+				catch (Exception exp){
+					MessageBox.Show("Error while saving keyboard configuration: \n" + exp,"Error while saving",MessageBoxButtons.OK,MessageBoxIcon.Error);
+					File.Copy(copypath,savepath,true);//if something goes wrong we restore the previously copied file
+				}
+			}
+		}
+		//Changing what type of events will be deleted
+		void Rdb_type1Click(object sender, EventArgs e)
+		{
+			if (rdb_type1.Checked == true){
+				type = 1;
+			}
+		}
+		void Rdb_type2Click(object sender, EventArgs e)
+		{
+			if (rdb_type2.Checked == true){
+				type = 2;
+			}
+		}
+		void Rdb_type3Click(object sender, EventArgs e)
+		{
+			if (rdb_type3.Checked == true){
+				type = 3;
+			}
+		}
+		void Chb_splitCheckedChanged(object sender, EventArgs e)
+		{
+			split = chb_split.Checked;
+		}
+		//deciding what to do when (a) key is pressed 
+		void MainFormKeyDown(object sender, KeyEventArgs e)
+		{
+			//if the settings menu is NOT active
+			if (settkey == false){
+				if ((e.KeyCode.ToString() == shortcuts[0].keybase) && (e.Modifiers.ToString() == shortcuts[0].keymod)) {//split
+						chb_split.Checked = !chb_split.Checked;
+						split = !split;
+				}
+				else if ((e.KeyCode.ToString() == shortcuts[1].keybase) && (e.Modifiers.ToString() == shortcuts[1].keymod)) {//just audio
+					rdb_type1.Checked = true;
+					type = 1;
+					End_Script();
+				}
+				else if ((e.KeyCode.ToString() == shortcuts[2].keybase) && (e.Modifiers.ToString() == shortcuts[2].keymod)) {//jsut video
+					rdb_type2.Checked = true;
+					type = 2;
+					End_Script();
+				}
+				else if ((e.KeyCode.ToString() == shortcuts[3].keybase) && (e.Modifiers.ToString() == shortcuts[3].keymod)) {//both
+					rdb_type3.Checked = true;
+					type = 3;
+					End_Script();
+				}
+			}
+			//if it is
+			else{
+				keymodact = e.Modifiers.ToString();
+				keybaseact = e.KeyCode.ToString();
+				tbx_preview.Text = keymodact + " + " + keybaseact;
+			}
+			
+		}
+		//changing the background color of the settings button
+		void Pbt_settingMouseEnter(object sender, EventArgs e)
+		{
+			pbt_setting.BackColor = SystemColors.Control;
+		}
+		void Pbt_settingMouseLeave(object sender, EventArgs e)
+		{
+			pbt_setting.BackColor = SystemColors.ControlDarkDark;
+		}
+		//opening 'n closing the settings menu
+		void Pbt_settingClick(object sender, EventArgs e)
+		{
+			if (settkey == false){
+				this.Width = 498;
+			}
+			else{
+				this.Width = 312;
+			}
+			settkey = !settkey;
+		}
+		//switching the focus when the user selects an item
+		void Cbb_selectshortSelectedIndexChanged(object sender, EventArgs e)
+		{
+			this.ActiveControl = btn_savekey;
+		}
+
+		
+
+		
+
+
+	}
+	public class EntryPoint{
+		
+		bool vg_split = false;
+		int vg_type = 3;
+		DialogResult result;
+		//opening the window and getting the values
+		public void FromVegas(Vegas myVegas){
+			using (MainForm form1 = new MainForm()){
+				result = form1.ShowDialog();
+				if (result == DialogResult.OK){
+					vg_type = form1.type;
+					vg_split = form1.split;
+				}
+			}
+			//deleting every event under the cursor
+			if (result == DialogResult.OK){
+				foreach (Track myTrack in myVegas.Project.Tracks) {
+					foreach (TrackEvent myEvent in myTrack.Events) {
+						if ((myEvent.Start <= myVegas.Cursor) && (myEvent.End > myVegas.Cursor)){
+							switch (vg_type) {
+								case 1:
+									if (myEvent.MediaType == MediaType.Audio){
+										if ((vg_split == true) && (myEvent.Start != myVegas.Cursor)){
+											AudioEvent newaud = (AudioEvent)myEvent.Split(myVegas.Cursor-myEvent.Start);
+											myTrack.Events.Remove(newaud);
+										}
+										else{
+											myTrack.Events.Remove(myEvent);
+										}
+									}
+								break;
+								case 2:
+									if (myEvent.MediaType == MediaType.Video){
+										if ((vg_split == true) && (myEvent.Start != myVegas.Cursor)){
+											VideoEvent newvid = (VideoEvent)myEvent.Split(myVegas.Cursor-myEvent.Start);
+											myTrack.Events.Remove(newvid);
+										}
+										else{
+											myTrack.Events.Remove(myEvent);
+										}
+									}
+								break;
+								case 3:
+									if ((vg_split == true) && (myEvent.Start != myVegas.Cursor)){
+										TrackEvent newevent = myEvent.Split(myVegas.Cursor-myEvent.Start);
+										myTrack.Events.Remove(newevent);
+									}
+									else{
+										myTrack.Events.Remove(myEvent);
+									}
+								break;
+								default:
+									MessageBox.Show("No event type selected. Select video, audio or both!","TypeError",MessageBoxButtons.OK,MessageBoxIcon.Error);
+								break;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+}
